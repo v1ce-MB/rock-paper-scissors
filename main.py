1b364df47acaf176271c7538a8d47bfb115258ba
v1ce-MB <Mburuvict@Gmail.com>
2024-11-23 19:55:24 +0300

    rock paper scissors

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+
+options = ("rock", "paper","scissors")
+player2= None
+computer = random.choice(options)
+
+while player2 not in options:
+   
+   player2= input("Enter a choice (rock, paper, scissors): ")
+
+print(f"(Player2) : {player2}")
+print(f"(Computer) : {computer}")
