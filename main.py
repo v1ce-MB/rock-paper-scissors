721405c0cbee927eb70609c9bbc2177b115ef8d1
v1ce-MB <Mburuvict@Gmail.com>
2024-12-20 18:53:07 +0300

    rock paper scissors

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ computer = random.choice(options)
 
 while player2 not in options:
    
-   player2= input("Enter a choice (rock, paper, scissors): ")
+    player2=input("Enter a choice (rock, paper, scissors): ")
 
 print(f"(Player2) : {player2}")
 print(f"(Computer) : {computer}")
